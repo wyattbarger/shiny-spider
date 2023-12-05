079814ffdbf536e84c932c3c0fd86a521487c6c9
Wyatt Barger <wyattbarger@icloud.com>
2023-12-05 09:25:15 -0600

    Add the beginning of the tickerArray, this will be hardcoded to reduce complexity and ensure ease of use. These will need to be updates as changes to the S&P 500 index are made, and noted in the version releases.

--- utils/tickerArray.js
+++ utils/tickerArray.js
@@ -1,1 +1,7 @@
 // Add an array that defines the ticker symbols that we will be using to make html requests with Axios to the NYSE website.
+const tickerArray = [
+    
+]
+
+
+module.exports = tickerArray;
