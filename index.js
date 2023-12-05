6aa068d425390bfba7e4e3009f94a489d7decaab
Wyatt Barger <wyattbarger@icloud.com>
2023-12-05 10:15:00 -0600

    Remove the tickerArray.js import from the main index.js file, as the proper path would have been for the index.js file in the utils directory to import all of the utils files, and them import the utils folder as a whole from the main index.js, as we have index.js set as our entry point in the package.json file.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 // Add require statements for each of the necessary technologies and utilities to build the scraper.
 const axios = require('axios');
 const cheerio = require('cheerio');
-const tickerArray = require('./utils/tickerArray');
---
+++ utils/index.js
@@ -0,0 +1,4 @@
+// Add the require statements for each utils file to the index.js for modularization and cleaner code.
+const tickerArray = require('./tickerArray');
+
+module.exports = {tickerArray, };
