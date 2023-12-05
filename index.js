efb0018dfb18f47affb5dc9858792e4c949f94b1
Wyatt Barger <wyattbarger@icloud.com>
2023-12-05 10:09:16 -0600

    Add the current installed technologies, and completed util file (tickerArray.js) to the main index.js file the scraper will be developed in.

---
+++ index.js
@@ -0,0 +1,4 @@
+// Add require statements for each of the necessary technologies and utilities to build the scraper.
+const axios = require('axios');
+const cheerio = require('cheerio');
+const tickerArray = require('./utils/tickerArray');
