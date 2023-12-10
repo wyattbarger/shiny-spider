aa616ae7b02673cc1d8552825e20614ea59eeef5
Wyatt Barger <wyattbarger@icloud.com>
2023-12-10 12:04:34 -0600

    Added the require statements for spiderLogic in the index.js file handling modularization within the utils directory.

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,5 @@
 // Add the require statements for each utils file to the index.js for modularization and cleaner code.
+const spiderLogic = require('./spiderLogic');
 const tickerArray = require('./tickerArray');
 
-module.exports = {tickerArray, };
+module.exports = { tickerArray, spiderLogic };
